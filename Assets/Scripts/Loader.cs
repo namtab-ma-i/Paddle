60f72d5ed8b43cbb0b676e58b097fedf964c5420
namtab-ma-i <chuiko.vera@gmail.com>
2018-01-17 15:26:02 +0200

    Added loading screen between levels

--- Assets/Scripts/Loader.cs
+++ Assets/Scripts/Loader.cs
@@ -8,14 +8,34 @@ using UnityEngine.SceneManagement;
 public class Loader : MonoBehaviour
 {
     public static int Level = 1;
+    private static Loader _instance;
+    
+    void Awake()
+    {
+        _instance = this;
+    }
 
     public static void Load(int level)
     {
-        //todo: show loading scene 
         Level = level;
-        SceneManager.LoadScene (0);
+        Debug.Log(Level);
+        if (_instance != null)
+        {
+            SceneManager.LoadScene (3);
+        }
+        else
+        {
+            SceneManager.LoadScene (0);
+        }
+        
     }
     
+    IEnumerator LoadScreen()
+    {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene (0);
+    }
+
     void OnEnable()
     {
         SceneManager.sceneLoaded += OnSceneLoaded;
@@ -23,6 +43,14 @@ public class Loader : MonoBehaviour
     
     void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
+        if (scene.buildIndex == 3)
+        {
+            TextMesh levelNum = GameObject.Find("/Number").GetComponent<TextMesh>();
+            levelNum.text = "" + Level;
+            _instance.StartCoroutine(_instance.LoadScreen());
+            return;
+        }
+        
         string path = "Assets/Levels/level" + Level + ".json"; //todo: encrypt & decrypt file!!!
         StreamReader reader = new StreamReader(path);
         string jsonData = reader.ReadToEnd();
@@ -36,7 +64,7 @@ public class Loader : MonoBehaviour
         Level levelScript = levelObj.GetComponent<Level>();
         levelScript.points = conf.points;
         levelScript.currentLevel = Level;
-        levelScript.playerScore = GameObject.Find("/DeadZone/Back");
+        levelScript.playerScore = GameObject.Find("/DeadZone/Back"); // setting score objects for dynamical update
         levelScript.enemyScore = GameObject.Find("/DeadZone/Front");
         
         GameObject enemyObj = GameObject.FindGameObjectWithTag ("Enemy");
